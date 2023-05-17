01186ccfa13dcfe941e436b02b81bbeca2ef8676
Dave Pearson <davep@davep.org>
2023-05-17 20:48:19 +0100

    :card_index: Export the direction navigation control

--- textual_fspicker/parts/__init__.py
+++ textual_fspicker/parts/__init__.py
@@ -1,3 +1,13 @@
 """Defines the parts that make up the filesystem picker dialogs."""
 
+##############################################################################
+# Local imports.
+from .directory_navigation import DirectoryNavigation
+
+##############################################################################
+# Export public items.
+__all__ = [
+    "DirectoryNavigation"
+]
+
 ### __init__.py ends here
