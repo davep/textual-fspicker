c4da1baced2a803d125a2f5fb57a5f3501f1f44f
Dave Pearson <davep@davep.org>
2023-05-18 20:46:16 +0100

    :books: Tidy up the example app

--- textual_fspicker/__main__.py
+++ textual_fspicker/__main__.py
@@ -1,21 +1,34 @@
+"""Main entry point for testing the library."""
+
+##############################################################################
+# Python imports.
 from pathlib import Path
 
+##############################################################################
+# Textual imports.
 from textual.app     import App, ComposeResult
 from textual.widgets import Label, Button
 
-from textual_fspicker.file_open import FileOpen
+##############################################################################
+# Local imports.
+from textual_fspicker import FileOpen
 
+##############################################################################
 class TestApp( App[ None ] ):
+    """A simple test application."""
 
     def compose( self ) -> ComposeResult:
         yield Label( "Press the button to pick something" )
         yield Button( "Select a file" )
 
-    def _open_file( self, to_open: Path ) -> None:
+    def show_selected( self, to_open: Path ) -> None:
         self.query_one( Label ).update( str( to_open ) )
 
     def on_button_pressed( self ):
-        self.push_screen( FileOpen("."), callback=self._open_file )
+        self.push_screen( FileOpen( "." ), callback=self.show_selected )
 
+##############################################################################
 if __name__ == "__main__":
     TestApp().run()
+
+### __main__.py ends here
