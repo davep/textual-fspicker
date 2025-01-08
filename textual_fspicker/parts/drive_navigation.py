af47cef06d4200de3780452569dcd44d84e40981
David Fokkema <davidfokkema@icloud.com>
2025-01-08 15:45:48 +0100

    WIP: drive navigation widget for Windows

---
+++ textual_fspicker/parts/drive_navigation.py
@@ -0,0 +1,37 @@
+from textual.widgets import OptionList, Static
+from textual.widgets.option_list import Option
+from textual.app import App, ComposeResult
+from textual.message import Message
+import pathlib
+from textual import on
+from dataclasses import dataclass
+import os
+
+
+class DriveNavigation(OptionList):
+    @dataclass
+    class DriveSelected(Message):
+        drive_root: str
+
+    def on_mount(self) -> None:
+        self.add_options(os.listdrives())
+
+    @on(OptionList.OptionSelected)
+    def select_drive(self, event: OptionList.OptionSelected) -> None:
+        self.post_message(self.DriveSelected(drive_root=event.option.prompt))
+
+
+class TestApp(App[None]):
+    def compose(self) -> ComposeResult:
+        yield DriveNavigation()
+        yield Static("No drive selected", id="drive_contents")
+
+    @on(DriveNavigation.DriveSelected)
+    def show_drive_contents(self, event: DriveNavigation.DriveSelected) -> None:
+        self.query_one("#drive_contents", Static).update(
+            "\n".join([str(p) for p in pathlib.Path(event.drive_root).glob("*")])
+        )
+
+
+if __name__ == "__main__":
+    TestApp().run()
