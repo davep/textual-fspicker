a23c4b63a151614bc19439bd3f347a852a542f68
David Fokkema <davidfokkema@icloud.com>
2025-01-09 23:10:56 +0100

    Undo import reordering

--- textual_fspicker/parts/directory_navigation.py
+++ textual_fspicker/parts/directory_navigation.py
@@ -8,6 +8,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
 from typing import ClassVar, Iterable, NamedTuple, Optional
+from typing_extensions import Final
 
 ##############################################################################
 # Rich imports.
@@ -24,14 +25,12 @@ from textual.reactive import var
 from textual.widgets import OptionList
 from textual.widgets.option_list import Option
 from textual.worker import get_current_worker
-from typing_extensions import Final
-
-from ..path_filters import Filter
-from ..path_maker import MakePath
 
 ##############################################################################
 # Local imports.
 from ..safe_tests import is_dir, is_file, is_symlink
+from ..path_filters import Filter
+from ..path_maker import MakePath
 
 
 ##############################################################################
