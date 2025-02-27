a0d2f9df5c751a721f8da74e85659d55e9a1c368
Dave Pearson <davep@davep.org>
2025-02-27 22:42:40 +0000

    :books: Some documentation tidying

--- src/textual_fspicker/safe_tests.py
+++ src/textual_fspicker/safe_tests.py
@@ -3,7 +3,8 @@
 This module provides some functions for performing tests on entries in the
 filesystem in a way that swallows some errors that we don't want causing the
 application to crash on. In most cases this is going to be a
-PermissionError, which for UI stuff is better ignored than raised.
+[`PermissionError`][PermissionError], which for UI stuff is better ignored
+than raised.
 """
 
 ##############################################################################
@@ -13,18 +14,17 @@ from pathlib import Path
 
 ##############################################################################
 def is_dir(location: Path) -> bool:
-    """A safe version of is_dir.
+    """A safe version of [`is_dir`][pathlib.Path.is_dir].
 
     Args:
         location: The location to test.
 
     Returns:
-        `True` if the location looks like a directory, `False` if not, or if
-        it could not be determined.
+        [`True`][True] if the location looks like a directory,
+            [`False`][False] if not, or if it could not be determined.
 
-    Note:
-        This function swallows `PermissionError` and just returns that the
-        location isn't a directory.
+    This function swallows [`PermissionError`][PermissionError] and just
+    returns that the location isn't a directory.
     """
     try:
         return location.is_dir()
@@ -34,18 +34,18 @@ def is_dir(location: Path) -> bool:
 
 ##############################################################################
 def is_file(location: Path) -> bool:
-    """A safe version of is_file.
+    """A safe version of [`is_file`][pathlib.Path.is_file].
 
     Args:
         location: The location to test.
 
     Returns:
-        `True` if the location looks like a file or if it could not be
-        determined, `False` it can be known it doesn't look like a file.
+        [`True`][True] if the location looks like a file or if it could not
+            be determined, [`False`][False] it can be known it doesn't look like
+            a file.
 
-    Note:
-        This function swallows `PermissionError` and just returns that the
-        location is a file.
+    This function swallows [`PermissionError`][PermissionError] and just
+    returns that the location is a file.
     """
     try:
         return location.is_file()
@@ -55,18 +55,17 @@ def is_file(location: Path) -> bool:
 
 ##############################################################################
 def is_symlink(location: Path) -> bool:
-    """A safe version of is_symlink.
+    """A safe version of [`is_symlink`][pathlib.Path.is_symlink].
 
     Args:
         location: The location to test.
 
     Returns:
-        `True` if the location looks like a symlink, `False` if not, or if
-        it could not be determined.
+        [`True`][True] if the location looks like a symlink,
+            [`False`][False] if not, or if it could not be determined.
 
-    Note:
-        This function swallows `PermissionError` and just returns that the
-        location isn't a symlink.
+    This function swallows [`PermissionError`][PermissionError] and just
+    returns that the location isn't a symlink.
     """
     try:
         return location.is_symlink()
